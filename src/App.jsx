37c19a98682d827c9b3f5b1572584b180b29773b
fandok <fandika.okdiba@gmail.com>
2024-01-19 13:58:50 +0700

    add emoji file and emoji filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { useMemo, useState } from "react";
+import { Container, List } from "reactstrap";
+
+import emojiList from "./emojiList.json";
+
 const App = () => {
-  return <div>App</div>;
+  const [search, setSearch] = useState("");
+
+  const filteredEmojiList = useMemo(() => {
+    if (search) {
+      return emojiList.filter((val) => {
+        const loweredCaseSearch = search.toLowerCase();
+        return (
+          val.title.toLowerCase().includes(loweredCaseSearch) ||
+          val.keywords.includes(loweredCaseSearch)
+        );
+      });
+    } else {
+      return emojiList;
+    }
+  }, [search]);
+
+  return (
+    <Container fluid="sm">
+      <h1 style={{ textAlign: "center" }}>Emoji Search</h1>
+      <input
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        style={{ width: "100%" }}
+      />
+      <List>
+        {filteredEmojiList.map((val) => (
+          <div key={val.title}>
+            {val.symbol} {val.title}
+          </div>
+        ))}
+      </List>
+    </Container>
+  );
 };
 
 export default App;
